b9eced5762ab44c7f3bae434a3ca447dd07d6b3c
mr.chaiwat chainvong <65877075+nui4328@users.noreply.github.com>
2023-07-23 18:30:53 +0700

    Update funtion.ino

--- local_robot1_3/funtion.ino
+++ local_robot1_3/funtion.ino
@@ -41,7 +41,7 @@ void fw_ch_can (int ml, int mr, float kp, int tim , int ofset)
                     previous_error=errors  ;  
                     PID_output = (kp * P) + (0.00015 * I) + (kp* D); 
         
-                    Motor(15 + PID_output, 15 - PID_output);
+                    Motor(10 + PID_output, 10 - PID_output);
                     //Serial.println(errors);
                     if(mcp_f(3) > md_mcp_f(3) && mcp_f(4) > md_mcp_f(4))
                       {
@@ -85,7 +85,8 @@ void fw_a3_a4(int ml, int mr, float kp, int ofset)
               {
                 break;
               }
-          }  
+          } 
+         
         if(ofset > 0)
            {
               Motor(-ml,-mr);delay(ofset);
@@ -96,7 +97,121 @@ void fw_a3_a4(int ml, int mr, float kp, int ofset)
            } 
                  
     }
+void fw_2sensor(int sl, int sr, float kp, int tm, String line,String sensor, int ofset)
+  {
+    char sensors[4];  // Declare a char array to store the converted string
+    line.toCharArray(sensors, sizeof(sensors));
+    int sensor_l = atoi(&sensors[0]); 
+    int sensor_r = atoi(&sensors[2]); 
+    if(tm > 0)
+      {
+        unsigned long lasts_time = millis();
+        while(millis() - lasts_time < tm)
+          {
+            float error_L = map(mcp_f(sensor_l), min_mcp_f(sensor_l), max_mcp_f(sensor_l), 0, 30 );
+            float error_R = map(mcp_f(sensor_r), min_mcp_f(sensor_r), max_mcp_f(sensor_r), 0, 30 );
+            errors = error_L - error_R;  
+            I = 0;
+            previous_I = 0;
+            previous_error = 0;
+            P = errors;
+            I = I + previous_I;
+            D = errors - previous_error ;            
+            previous_I=I;
+            previous_error=errors  ;  
+            PID_output = (kp * P) + (0.00015 * I) + (kp* D); 
 
+            Motor(sl + PID_output, sr - PID_output);
+            //Serial.println(errors);
+  
+          }
+          
+      }
+    else
+      {
+        while(1)
+          {
+            float error_L = map(mcp_f(sensor_l), min_mcp_f(sensor_l), max_mcp_f(sensor_l), 0, 30 );
+            float error_R = map(mcp_f(sensor_r), min_mcp_f(sensor_r), max_mcp_f(sensor_r), 0, 30 );
+            errors = error_L - error_R;  
+            I = 0;
+            previous_I = 0;
+            previous_error = 0;
+            P = errors;
+            I = I + previous_I;
+            D = errors - previous_error ;            
+            previous_I=I;
+            previous_error=errors  ;  
+            PID_output = (kp * P) + (0.00015 * I) + (kp* D); 
+
+            Motor(sl + PID_output, sr - PID_output);
+            //Serial.println(errors);
+            if(mcp_f(2)< md_mcp_f(2)&&mcp_f(3)>md_mcp_f(3))
+              {
+                do{Motor(-10, 20);}while(mcp_f(3) > md_mcp_f(3));
+              }
+            if(mcp_f(5)< md_mcp_f(5)&&mcp_f(4)>md_mcp_f(4))
+              {
+                do{Motor(20, 10);}while(mcp_f(4) > md_mcp_f(4));
+              }
+            if(sensor == "a0")
+                  {
+                      if(mcp_f(0)<md_mcp_f(0))
+                          {
+                              break;
+                          }
+                  }
+              else if(sensor == "a7")
+                  {
+                      if(mcp_f(7)<md_mcp_f(7))
+                          {
+                              break;
+                          }
+                  }
+             if(sensor == "b1")
+                  {
+                      if(mcp_b(0)<md_mcp_b(0))
+                          {
+                              break;
+                          }
+                  }
+              else if(sensor == "b6")
+                  {
+                      if(mcp_b(7)<md_mcp_b(7))
+                          {
+                              break;
+                          }
+                  }
+             if(sensor == "26")
+                  {
+                      if(analogRead(26)<md_adc(26))
+                          {
+                              break;
+                          }
+                  }
+              else if(sensor == "27")
+                  {
+                      if(analogRead(27)<md_adc(27))
+                          {
+                              break;
+                          }
+                  }
+
+          }
+      }
+
+ ///////////////////////////////////////////////////////////////     
+
+    if(ofset > 0)
+      {
+        Motor(-sl, -sr);
+        delay(ofset);
+        Motor(0, 0);
+        delay(50);
+      }
+    else  {}
+ //////////////////////////////////////////////////////////////     
+  }
 void bw_a3_a4(int ml, int mr, float kp, int ofset)
     {
        while(1)
@@ -130,46 +245,6 @@ void bw_a3_a4(int ml, int mr, float kp, int ofset)
         else
            {
            }          
-    }
-
-void fw_stop_center()
-    {
-      while(1)
-          {
-            int error_L = map(mcp_f(3), min_mcp_f(3), max_mcp_f(3), 0, 20 );
-            int error_R = map(mcp_f(4), min_mcp_f(4), max_mcp_f(4), 0, 20 );
-            errors = error_L - error_R;  
-            I = 0;
-            previous_I = 0;
-            previous_error = 0;
-            P = errors;
-            I = I + previous_I;
-            D = errors - previous_error ;            
-            previous_I=I;
-            previous_error=errors  ;  
-            PID_output = (0.2 * P) + (0.00015 * I) + (0.5* D); 
-
-            Motor(30 + PID_output, 30 - PID_output);
-            Serial.println(errors);
-            if(analogRead(26) < md_adc(26)&& analogRead(27) < md_adc(27) )
-              {
-                break;
-              }
-          } 
-         Motor(-10, -10); delay(10); 
-         Motor(0, 0); delay(10);   
-         if (mcp_f(3) > md_mcp_f(3) && mcp_f(4) < md_mcp_f(4))
-            {
-              do{Motor(30, -30);}while(mcp_f(3) > md_mcp_f(3)-150);
-              Motor(-20, 20); delay(10); 
-              Motor(0, 0); delay(200);    
-            }
-          else if (mcp_f(4) > md_mcp_f(4) && mcp_f(3) < md_mcp_f(3))
-            {
-              do{Motor(-30, 30);}while(mcp_f(4) > md_mcp_f(4)-150);
-              Motor(20, -20); delay(10); 
-              Motor(0, 0); delay(200);    
-            }
     }
 
 
@@ -237,7 +312,7 @@ void tx_left(int sp, int ofset)
        do{Motor(-sp, sp);}while(mcp_f(3) > md_mcp_f(3)-100);delay(10);
        
        Motor(sp, -sp); delay(ofset); 
-       Motor(0, 0); delay(200);
+       Motor(0, 0); delay(50);
     }
 void tx_right(int sp, int ofset)
     {
@@ -249,5 +324,5 @@ void tx_right(int sp, int ofset)
        do{Motor(sp, -sp);}while(mcp_f(4) > md_mcp_f(4)-100);delay(10);
        
        Motor(-sp, sp); delay(ofset); 
-       Motor(0, 0); delay(200);
+       Motor(0, 0); delay(50);
     }
