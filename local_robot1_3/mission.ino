4f9c99f230699debe18197c2ff7c09d42b7319ec
mr.chaiwat chainvong <65877075+nui4328@users.noreply.github.com>
2023-07-23 18:31:32 +0700

    Update mission.ino

--- local_robot1_3/mission.ino
+++ local_robot1_3/mission.ino
@@ -14,7 +14,7 @@ void check_box()
 void fl()
   {
     fline(40,40, 0.3, 0, 'c', 'l',50, "a2", 30);    
-    fline(50,50, 0.3, 0, 'c', 's',60, "a7", 12); 
+    fline(40,40, 0.3, 0, 'c', 's',60, "a7", 12); 
     do{Motor(50, -20);}while(mcp_f(4) > md_mcp_f(4)-150);
     Motor(-40, 20); delay(20); 
     Motor(0, 0); delay(100); 
@@ -41,13 +41,13 @@ void fl()
           servo_store();
           tll = 0; 
         }
-     bline(50,50, 0.1, 0, 'f', 's',50, "a5", 10); 
-     do{Motor(-30, -30);}while(analogRead(27) > md_adc(27));
+     bline(40,40, 0.1, 0, 'f', 's',50, "a5", 10); 
+     do{Motor(-40, -40);}while(analogRead(27) > md_adc(27));
      Motor(20, 20); delay(10); 
      Motor(0, 0); delay(10); 
       
-     do{Motor(50, -50);}while(mcp_f(7) > md_mcp_f(7));
-     do{Motor(50, -50);}while(mcp_f(4) > md_mcp_f(4));
+     do{Motor(40, -40);}while(mcp_f(7) > md_mcp_f(7));
+     do{Motor(40, -40);}while(mcp_f(4) > md_mcp_f(4));
      Motor(-50, 50); delay(20); 
      Motor(0, 0); delay(10);
      
@@ -58,7 +58,7 @@ void fl()
  void  fr()
   {
     fline(40,40, 0.3, 0, 'c', 'r',50, "a5", 30);    
-    fline(50,50, 0.3, 0, 'c', 's',60, "a7", 12); 
+    fline(40,40, 0.3, 0, 'c', 's',60, "a7", 12); 
     do{Motor(-20, 50);}while(mcp_f(3) > md_mcp_f(3)-150);
     Motor(20, -40); delay(20); 
     Motor(0, 0); delay(100); 
@@ -85,14 +85,14 @@ void fl()
           trr = 0; 
         }
      
-     bline(50,50, 0.1, 0, 'f', 's',50, "a5", 10); 
-     do{Motor(-30, -30);}while(analogRead(26) > md_adc(26));
+     bline(40,40, 0.1, 0, 'f', 's',50, "a5", 10); 
+     do{Motor(-40, -40);}while(analogRead(26) > md_adc(26));
      Motor(20, 20); delay(10); 
      Motor(0, 0); delay(10);
        
-     do{Motor(-50, 50);}while(mcp_f(0) > md_mcp_f(0));
-     do{Motor(-50, 50);}while(mcp_f(3) > md_mcp_f(3));
-     Motor(50, -50); delay(20); 
+     do{Motor(-40, 40);}while(mcp_f(0) > md_mcp_f(0));
+     do{Motor(-40, 40);}while(mcp_f(3) > md_mcp_f(3));
+     Motor(50, -40); delay(20); 
      Motor(0, 0); delay(10);
       
      fline(50,50, 0.2, 0, 'c', 'l',50, "a3", 30);  //-------> จบตรงสี่แยกหน้าจุดเริ่มต้น
